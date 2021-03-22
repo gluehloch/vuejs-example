d561baa4394665c8a29a695cb6c706e3a79a6b47
Andre Winkler <gluehloch@googlemail.com>
2021-03-22 06:35:50 +0100

    Adding routing to the example app.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,138 +2,50 @@
     <div class="hello">
         <h1>{{ msg }}</h1>
 
-        <hr/>
+        <hr />
 
-        <Register :pre-select="selection" v-on:update-selection="updateSelection"/>
-        <Register :pre-select="selection"/>
-        <SharedMessage :shared="shared" :sharedUpdateCounter="counter"/>
+        <Register
+            :pre-select="selection"
+            v-on:update-selection="updateSelection"
+        />
+        <Register :pre-select="selection" />
+        <SharedMessage :shared="shared" :sharedUpdateCounter="counter" />
 
-        <hr/>
-
-        <p>
-            For a guide and recipes on how to configure / customize this
-            project,<br />
-            check out the
-            <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-                >vue-cli documentation</a>.
-        </p>
-        <h3>Installed CLI Plugins</h3>
-        <ul>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-                    target="_blank"
-                    rel="noopener"
-                    >typescript</a>
-            </li>
-        </ul>
-        <h3>Essential Links</h3>
-        <ul>
-            <li>
-                <a href="https://vuejs.org" target="_blank" rel="noopener"
-                    >Core Docs</a>
-            </li>
-            <li>
-                <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-                    >Forum</a>
-            </li>
-            <li>
-                <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-                    >Community Chat</a>
-            </li>
-            <li>
-                <a
-                    href="https://twitter.com/vuejs"
-                    target="_blank"
-                    rel="noopener"
-                    >Twitter</a>
-            </li>
-            <li>
-                <a href="https://news.vuejs.org" target="_blank" rel="noopener"
-                    >News</a>
-            </li>
-        </ul>
-        <h3>Ecosystem</h3>
-        <ul>
-            <li>
-                <a
-                    href="https://router.vuejs.org"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-router</a>
-            </li>
-            <li>
-                <a href="https://vuex.vuejs.org" target="_blank" rel="noopener"
-                    >vuex</a>
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-devtools#vue-devtools"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-devtools</a>
-            </li>
-            <li>
-                <a
-                    href="https://vue-loader.vuejs.org"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-loader</a>
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/awesome-vue"
-                    target="_blank"
-                    rel="noopener"
-                    >awesome-vue</a>
-            </li>
-        </ul>
+        <hr />
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import { Shared } from './Shared';
-import Register from  './Register.vue';
-import SharedMessage from './SharedMessage.vue';
+import { Shared } from "./Shared";
+import Register from "./Register.vue";
+import SharedMessage from "./SharedMessage.vue";
 
-@Component({components: {
-        Register, SharedMessage
-    }
+@Component({
+    components: {
+        Register,
+        SharedMessage,
+    },
 })
 export default class HelloWorld extends Vue {
     @Prop() private msg!: string;
 
-    selection: string = '';
+    selection: string = "";
     shared: Shared = new Shared();
     counter = 0;
 
     updateSelection(selection: any) {
-        console.log('HelloWorld#updateSelection :: Message from component "register component":', selection, selection.key);
+        console.log(
+            'HelloWorld#updateSelection :: Message from component "register component":',
+            selection,
+            selection.key
+        );
         this.selection = selection.key;
 
         this.shared.id = this.counter++;
-        this.shared.name = 'Andre Winkler was here';
+        this.shared.name = "Andre Winkler was here";
 
-        console.log('HelloWorld#updateSelection :: Shared data: ', this.shared);
+        console.log("HelloWorld#updateSelection :: Shared data: ", this.shared);
     }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-    margin: 40px 0 0;
-}
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-a {
-    color: #42b983;
-}
-</style>
